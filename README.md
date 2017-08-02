c0f00ab760256c0da9004aa491d6b2d08dd9d37e
qgp9 <pauljang9@gmail.com>
2017-08-02 05:13:47 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # vuetalisk-plugin-mustache-simple
+
+## Install
+```
+npm i --save vuetalisk-plugin-mustache-simple
+```
+
+## Set up
+
+In `vuetalisk.config.js`
+
+```js
+const MustacheSimple = require('vuetalisk-plugin-mustache-simple')
+const words = ['base', 'github.repo', 'ga.id']
+
+const buildApi = () =>
+  init()
+    .use('file-loader')
+    .use('frontmatter')
+    .use('filename-handler')
+    .use('url-handler')
+    .use(MustacheSimple, words)
+    .use('data-handler')
+    .use('list-handler')
+    .use('api-writer')
+    .use('static-handler')
+    .use('cleaner')
+```
+
+## Usage
+
+https://github.com/qgp9/mustache-simple
---
+++ plugins.js
@@ -0,0 +1,28 @@
+const musta = require('mustache-simple')
+
+class MustacheSimple {
+  constructor (words) {
+    this.words = words 
+  }
+
+  async register ({h, options = {}}) {
+    const opts = options.MustacheSimple || {}
+    this.words = opts.words || this.words
+    this.config = opts.config || h.config.config
+    if (this.words && this.config) this.musta = musta.compileMatch(this.config, this.words)
+  }
+
+  async processItem ({h, item, options = {}}) {
+    const opts = options.MustacheSimple || {}
+    this.words = opts.words || this.words
+    this.config = opts.config || h.config.config
+    if (opts.words || opts.config) this.musta = musta.compileMatch(this.config, this.words)
+
+    if (item.data && item.data.constructor === String) item.data = this.musta(item.data)
+  }
+}
+
+module.exports = MustacheSimple
+MustachSimple.install = function (words) {
+  return new MustacheSimple(words)
+}
